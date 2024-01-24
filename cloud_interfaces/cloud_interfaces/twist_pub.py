ab012b2bdcad7ecf31b39d0ebad48f3eeab63305
Siddarh09 <siddarth.dayasagar@gmail.com>
2024-01-24 17:13:31 +0530

    updated

--- cloud_interfaces/cloud_interfaces/twist_pub.py
+++ cloud_interfaces/cloud_interfaces/twist_pub.py
@@ -3,28 +3,40 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 import json
 import websockets
-
+import asyncio
 
 class TwistPublisher(Node):
-    def __init__(self):
-        super().__init__("twist_pub")
+    def _init_(self):
+        super()._init_("twist_pub")
         self.pub = self.create_publisher(Twist, "/cmd_vel", 10)
         self.json_data={}
+        self.timer=self.create_timer(10.0,self.twist_pub)
 
-    async def get_data(self):
-        uri = "ws://172.25.60.254:8000"
-        self.get_logger().info(f"Connecting to {uri}")
-        try:
-            async with websockets.connect(uri) as websocket:
-                self.get_logger().info("Connected to WebSocket")
-                json_data = await websocket.recv()
+    # async def get_data(self):
+    #     uri = "ws://172.25.60.254:8000"
+    #     self.get_logger().info(f"Connecting to {uri}")
+    #     try:
+    #         async with websockets.connect(uri) as websocket:
+    #             self.get_logger().info("Connected to WebSocket")
+    #             json_data = await websocket.recv()
             
-        except websockets.exceptions.WebSocketException as e:
-            self.get_logger().error(f"WebSocket connection error: {str(e)}")
+    #     except websockets.exceptions.WebSocketException as e:
+    #         self.get_logger().error(f"WebSocket connection error: {str(e)}")
+
+    async def websocket_client(self,uri):
+        async with websockets.connect(uri) as websocket:
+            while True:
+                # Receive data from the WebSocket
+                    print("CONNECTED")
+                    data = await websocket.recv()
+                    self.json_data=data
+                    print(f"Received data from WebSocket: {data}")
+                    
             
            
 
     def twist_pub(self):
+        
         while rclpy.ok():
             data = self.json_data
             msg = Twist()
@@ -37,6 +49,8 @@ class TwistPublisher(Node):
 def main():
     rclpy.init(args=None)
     robot_vel = TwistPublisher()
+    websocket_uri = 'ws://172.25.60.254:8000'
+    asyncio.get_event_loop().run_until_complete(robot_vel.websocket_client(websocket_uri))
     try:
         rclpy.spin(robot_vel)
     except KeyboardInterrupt:
@@ -46,5 +60,5 @@ def main():
         rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     main()
